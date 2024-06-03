f974a51e380ff3e02970bf549b9de32a32615cb2
ProfJocile <profjocile@gmail.com>
2024-06-03 17:20:01 -0300

    conta iniciada

---
+++ Conta.py
@@ -0,0 +1,19 @@
+class conta:
+    def __init__(self, numero, cliente):
+        self._saldo = 0
+        self._numero = numero
+        self._agencia = "01"
+        self._cliente = cliente
+        self._historico = historico()
+    
+    def saldo():
+        pass
+
+    def sacar():
+        pass
+
+    def depositar():
+        pass
+
+    def criar_conta():
+        pass
